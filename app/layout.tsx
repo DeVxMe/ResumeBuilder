1683827c6cdb45270feb69f56faebefc63595de4
BiMalxMe <journeyxbimal@gmail.com>
2025-08-14 21:15:09 +0545

    feat : Icons and all

--- app/layout.tsx
+++ app/layout.tsx
@@ -98,7 +98,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${spaceGrotesk.variable} ${dmSans.variable} antialiased`}>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="../public/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
